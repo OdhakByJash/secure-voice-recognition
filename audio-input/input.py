0d884b90bc6655248dbf9ba8f6ce9f61bf9ed9ba
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-08-13 23:25:44 +0530

    Testing Audio Input

--- audio-input/input.py
+++ audio-input/input.py
@@ -1,10 +1,11 @@
 import sounddevice as sd
 def input_audio():
     sampling_frequency = 44100
-    time_period = 5
+    time_period = 15
     recording = sd.rec(
         int(sampling_frequency*time_period),
         samplerate=sampling_frequency,
-        channels=2
+        channels=1
     )
     return [sampling_frequency,time_period,recording]
+print(input_audio())
