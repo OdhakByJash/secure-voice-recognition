312d3a1e89feb9f1bcbca6f4cf7f2523cb498c8b
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-08-13 23:30:52 +0530

    Time Period Brought Down To 7 Seconds, Now Will Go And Research On Best Sampling Frequency And Time Period TO Get The Best Input Data

--- audio-input/input.py
+++ audio-input/input.py
@@ -1,7 +1,7 @@
 import sounddevice as sd
 def input_audio():
     sampling_frequency = 44100
-    time_period = 15
+    time_period = 7
     recording = sd.rec(
         int(sampling_frequency*time_period),
         samplerate=sampling_frequency,
