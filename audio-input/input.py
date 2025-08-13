68bf2ce07a5eaad6eff8e3fd76375ed5dfdd439e
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-08-13 23:35:14 +0530

    Sampling Frequency Set To Capture At Best Quality

--- audio-input/input.py
+++ audio-input/input.py
@@ -1,6 +1,6 @@
 import sounddevice as sd
 def input_audio():
-    sampling_frequency = 44100
+    sampling_frequency = 48000
     time_period = 7
     recording = sd.rec(
         int(sampling_frequency*time_period),
