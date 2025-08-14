af9ef0b98f9da844e52305e166448dbd7f7d006e
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-08-14 16:36:25 +0530

    Delete Endpoint Written

--- voicever/user/views.py
+++ voicever/user/views.py
@@ -1,7 +1,22 @@
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.status import HTTP_401_UNAUTHORIZED,HTTP_200_OK
 @api_view(['POST'])
 def create(request):
     pass
 @api_view(['POST'])
 def delete(request):
-    pass
+    try:
+        request.user.delete()
+        return Response(
+            {
+                "Status":"Account Deleted Successfully"
+            },status=HTTP_200_OK
+        )
+    except Exception as e:
+        return Response(
+            {
+                "Status":"Error",
+                "Error":str(e)
+            },status=HTTP_401_UNAUTHORIZED
+        )
