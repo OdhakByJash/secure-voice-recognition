390861683471ad0125d00ad2fbe9ac14b4805e46
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-08-14 10:55:13 +0530

    User App Initialized And Registered

--- voicever/voicever/settings.py
+++ voicever/voicever/settings.py
@@ -13,6 +13,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'user'
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
