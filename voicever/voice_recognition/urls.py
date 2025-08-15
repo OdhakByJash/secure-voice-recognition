4a89b07d9a221af14db242520f444aa1be392ca4
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-08-15 12:09:36 +0530

    URLs File Written

--- voicever/voice_recognition/urls.py
+++ voicever/voice_recognition/urls.py
@@ -1,3 +1,6 @@
+from django.urls import path
+from voice_recognition.views import register,verify
 urlpatterns = [
-    
+    path('register/',register,name='register'),
+    path('verify/',verify,name='verify')
 ]
