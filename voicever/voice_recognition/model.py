986ccf70cc0ce0a46669d3f521636b196e6cd186
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-08-15 11:44:14 +0530

    Model Written For Associating Voive Sample To A User And Storing It

--- voicever/voice_recognition/model.py
+++ voicever/voice_recognition/model.py
@@ -1,3 +1,13 @@
-from django.db.models import Model
+from django.db.models import Model,FileField,ForeignKey,CASCADE,UniqueConstraint
+from django.contrib.auth.models import User
 class AuthenticUserVoice(Model):
-    pass
+    sample = FileField(upload_to="voice")
+    user = ForeignKey(User,related_name="voice",on_delete=CASCADE)
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                fields=['sample','user'],
+                name='one-sample-one-user-unique'
+            )
+        ]
+    
