f3b55416b1b7fd3bc6ebdb99d0ad4ba67bf1a3ad
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-08-19 11:51:10 +0530

    Throttling Added, PHASE 1 COMPLETED

--- voicever/voicever/settings.py
+++ voicever/voicever/settings.py
@@ -74,5 +74,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES':[
         'rest_framework.authentication.TokenAuthentication'
-    ]
+    ],
+    'DEFAULT_THROTTLE_CLASSES':[
+        'rest_framework.throttling.AnonRateThrottle'
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES':{
+        'anon':'30/min',
+        'user':'60/min'
+    }
 }
